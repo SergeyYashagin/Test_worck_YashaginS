7aeb4f7a7f2cf0f144a8ab970cfc99eb646882de
Sergey <seregaman89@gmail.com>
2022-07-26 06:19:53 +0400

    Create file Program.cs with cod of programm

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] mass1 = new string[3] {"hello", "2", ";-)"};
+string[] mass2 = new string[mass1.Length];
+int count = 0;
+void CheckMass(string[] mass1, string[] mass2)
+{
+    for (int i = 0; i < mass1.Length; i++)
+    {
+        if (mass1[i].Length <= 3)
+        {
+            mass2[count] = mass1[i];
+            count++;
+        }
+    }
+}
+
+CheckMass(mass1, mass2);
+
+for (int i = 0; i < mass2.Length; i++)
+{
+    Console.Write($"{mass2[i]} ");
+}
